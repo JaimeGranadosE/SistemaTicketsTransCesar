670c84a426035efb652a35519111bce3dd2e6a07
Jaime Granados <jaimelcartero99@gmail.com>
2026-03-20 18:04:03 -0500

    feat: implemente los servios base para lógica del sistema

--- SistemaTicketsTransCesar/src/view/Main.java
+++ SistemaTicketsTransCesar/src/view/Main.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package view;
 
-/**
- *
- * @author HP
- */
 public class Main {
-    
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.mostrar();
+    }
 }
